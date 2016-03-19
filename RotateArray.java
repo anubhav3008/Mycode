7df76708fa08ebd126e17e9514f6d6fa9cabce1a
Anubhav Shrivastava <anubhav.workemail@gmail.com>
2016-03-19 17:48:27 +0530

    Adding two more very good algorithms for this

--- RotateArray.java
+++ RotateArray.java
@@ -35,3 +35,22 @@ public class RotateArray {
 	}
 
 }
+/* There exists one more method: resversal algorithm : http://www.geeksforgeeks.org/program-for-array-rotation-continued-reversal-algorithm/
+  rotate(arr[], d, n)
+  reverse(arr[], 1, d) ;
+  reverse(arr[], d + 1, n);
+  reverse(arr[], l, n);
+  
+ There is another approch: bock swap algorithm : http://www.geeksforgeeks.org/block-swap-algorithm-for-array-rotation/ :
+ Initialize A = arr[0..d-1] and B = arr[d..n-1]
+1) Do following until size of A is equal to size of B
+
+  a)  If A is shorter, divide B into Bl and Br such that Br is of same 
+       length as A. Swap A and Br to change ABlBr into BrBlA. Now A
+       is at its final place, so recur on pieces of B.  
+
+   b)  If A is longer, divide A into Al and Ar such that Al is of same 
+       length as B Swap Al and B to change AlArB into BArAl. Now B
+       is at its final place, so recur on pieces of A.
+
+2 Finally when A and B are of equal size, block swap them.
